d0dbf6e9c3afadee5c0b6a27455a753f2fc6fd9b
Konrad Heidler <k.heidler@tum.de>
2021-08-12 20:46:53 +0200

    Add WandB logging

---
+++ plotting.py
@@ -0,0 +1,41 @@
+import numpy as np
+import wandb
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+
+
+def draw_snake(draw, snake, dashed=False, **kwargs):
+    if dashed:
+        for (y0, x0, y1, x1) in snake.reshape((-1, 4)):
+            draw.line((x0, y0, x1, y1), **kwargs)
+    else:
+        for (y0, x0), (y1, x1) in zip(snake, snake[1:]):
+            draw.line((x0, y0, x1, y1), **kwargs)
+
+
+def log_image(img, snake, pred, init, tag, step):
+    H, W, C = img.shape
+    snake   = 0.5 * H * (1 + snake)
+    init    = 0.5 * H * (1 + init)
+    pred    = 0.5 * H * (1 + pred)
+
+    if C == 1:
+        RGB = [0, 0, 0]
+    elif C == 3:
+        RGB = [0, 1, 2]
+    else:
+        RGB = [3, 2, 1]
+
+    img = (255 * img[:,:,RGB]).astype(np.uint8)
+    img = Image.fromarray(img, mode='RGB')
+    draw = ImageDraw.Draw(img)
+
+    draw_snake(draw, init, fill=(0, 0, 255))
+    draw_snake(draw, snake, fill=(255, 0, 0), width=3)
+
+    kwargs = dict(fill=(0, 255, 0), width=3)
+    draw_snake(draw, pred, **kwargs)
+
+    img = np.asarray(img).astype(np.float32) / 255
+    print('Logging Img')
+    wandb.log({tag: wandb.Image(img)}, step=step)
--- train.py
+++ train.py
@@ -9,7 +9,10 @@ from functools import partial
 from models.deepsnake import DeepSnake, SimpleModel
 from typing import NamedTuple, Mapping
 
+import wandb
 from tqdm import trange
+
+from plotting import log_image
 
 BATCH_SIZE = 4
 
@@ -19,10 +22,19 @@ class TrainingState(NamedTuple):
     opt_state: optax.OptState
 
 
+def loss_fn(predictions, ground_truth):
+    loss_fwd = jnp.sum(jnp.square(predictions - ground_truth), axis=-1)
+    loss_bwd = jnp.sum(jnp.square(predictions[:, ::-1] - ground_truth), axis=-1)
+
+    loss = jnp.mean(jnp.minimum(loss_fwd, loss_bwd))
+
+    return loss
+
+
 def calculate_loss(params, net, imagery, contours):
     init_contours = jnp.roll(contours, 1, 0)
     predictions = net.apply(params, imagery, init_contours)
-    loss = jnp.mean(jnp.sum(jnp.square(predictions - contours), axis=-1))
+    loss = loss_fn(predictions, contours)
     return loss
 
 
@@ -35,7 +47,7 @@ def make_batch(key):
     return jax.vmap(generate_image)(data_keys)
 
 
-# @jax.jit
+@jax.partial(jax.jit, static_argnums=2)
 def train_step(state, key, net):
     imagery, contours = make_batch(key)
     _, optimizer = get_optimizer()
@@ -49,6 +61,15 @@ def train_step(state, key, net):
         params=new_params,
         opt_state=new_opt_state
     )
+
+
+@jax.partial(jax.jit, static_argnums=2)
+def val_step(state, key, net):
+    imagery, contours = make_batch(key)
+    init_contours = jnp.roll(contours, 1, 0)
+    predictions = net.apply(state.params, imagery, init_contours)
+    loss = loss_fn(predictions, contours)
+    return loss, imagery, contours, predictions, init_contours
 
 
 def forward_function(imagery, init):
@@ -66,20 +87,29 @@ def main():
     params = net.init(jax.random.PRNGKey(0), *make_batch(jax.random.PRNGKey(0)))
     opt_state = opt_init(params)
     state = TrainingState(params=params, opt_state=opt_state)
+    wandb.init(project='Deep Snake Pre-Train')
 
     for epoch in range(100):
-        prog = trange(100)
+        prog = trange(10000)
         losses = []
         for step in prog:
             train_key, subkey = jax.random.split(train_key)
             loss, state = train_step(state, subkey, net)
             losses.append(loss)
-            prog.set_description(f'{np.mean(losses):.3f}')
+            if step % 100 == 0:
+                prog.set_description(f'{np.mean(losses):.3f}')
+        wandb.log({f'trn/loss': np.mean(losses)}, step=epoch)
         # Validate
         val_key = persistent_val_key
+        losses = []
         for step in range(1):
             val_key, subkey = jax.random.split(val_key)
-            train_step(state, val_key, net)
+            loss, *inspection = val_step(state, val_key, net)
+            losses.append(loss)
+            for i in range(inspection[0].shape[0]):
+                print('logging img')
+                log_image(*[np.asarray(ary[i]) for ary in inspection], "", epoch)
+        wandb.log({f'val/loss': np.mean(losses)}, step=epoch)
 
 
 if __name__ == '__main__':
